3a5c64c2a008b6c7787f86e6c9d627799decc01f
CODE SEARCH <150044321+CodeSearchDev@users.noreply.github.com>
2024-12-24 21:35:28 +0530

    Update stickers.py

--- TanuMusic/plugins/tools/stickers.py
+++ TanuMusic/plugins/tools/stickers.py
@@ -1,14 +1,9 @@
-import base64
 import httpx
-import os
 from pyrogram import filters
-from config import BOT_USERNAME
-from TanuMusic import app
-from pyrogram import filters
-import pyrogram
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from uuid import uuid4
-from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
-
+from TanuMusic import app
+import pyrogram
 
 BUTTONS = [
     [
@@ -16,43 +11,10 @@ BUTTONS = [
     ],
 ]
 
-@app.on_message(filters.reply & filters.command("upscale"))
-async def upscale_image(client, message):
-    try:
-        if not message.reply_to_message or not message.reply_to_message.photo:
-            await message.reply_text("✦ ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ɪᴛ.")
-            return
-
-        image = message.reply_to_message.photo.file_id
-        file_path = await client.download_media(image)
-
-        with open(file_path, "rb") as image_file:
-            f = image_file.read()
-
-        b = base64.b64encode(f).decode("utf-8")
-
-        async with httpx.AsyncClient() as http_client:
-            response = await http_client.post(
-                "https://api.qewertyy.me/upscale", data={"image_data": b}, timeout=None
-            )
-
-        with open("upscaled_image.png", "wb") as output_file:
-            output_file.write(response.content)
-
-        await client.send_document(
-            message.chat.id,
-            document="upscaled_image.png",
-            caption="๏ ʜᴇʀᴇ ɪs ᴛʜᴇ ᴜᴘsᴄᴀʟᴇᴅ ɪᴍᴀɢᴇ !",
-        )
-
-    except Exception as e:
-        print(f"✦ ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ: {e}")
-        await message.reply_text("✦ ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ.")
-
 ######### sticker id
 
 @app.on_message(filters.command("kang"))
-async def _packkang(app :app,message):  
+async def _packkang(app, message):  
     txt = await message.reply_text("✦ ᴘʀᴏᴄᴇssɪɴɢ....")
     if not message.reply_to_message:
         await txt.edit('ʀᴇᴘʟʏ ᴛᴏ ᴍᴇssᴀɢᴇ')
@@ -64,7 +26,7 @@ async def _packkang(app :app,message):
         return await txt.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ɴᴏɴ-ᴀɴɪᴍᴀᴛᴇᴅ sᴛɪᴄᴋᴇʀ")
     if len(message.command) < 2:
         pack_name =  f'{message.from_user.first_name}_sticker_pack_by_@TanuMusicxBot'
-    else :
+    else:
         pack_name = message.text.split(maxsplit=1)[1]
     short_name = message.reply_to_message.sticker.set_name
     stickers = await app.invoke(
@@ -100,22 +62,22 @@ async def _packkang(app :app,message):
                 stickers=sticks,
             )
         )
-        await txt.edit(f"❖ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴋᴀɴɢᴇᴅ ʟɪɴᴋ !\n● ᴛᴏᴛᴀʟ sᴛɪᴄᴋᴇʀ ➥ {len(sticks)}",reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴘᴀᴄᴋ ʟɪɴᴋ",url=f"http://t.me/addstickers/{short_name}")]]))
+        await txt.edit(f"❖ ʜᴇʀᴇ ɪs ʏᴏᴜʀ ᴋᴀɴɢᴇᴅ ʟɪɴᴋ !\n● ᴛᴏᴛᴀʟ sᴛɪᴄᴋᴇʀ ➥ {len(sticks)}", reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("ᴘᴀᴄᴋ ʟɪɴᴋ", url=f"http://t.me/addstickers/{short_name}")]]))
     except Exception as e:
         await message.reply(str(e))
 
 
 ###### sticker id =
-@app.on_message(filters.command(["stickerid","stid"]))
-async def sticker_id(app: app, msg):
+@app.on_message(filters.command(["stickerid", "stid"]))
+async def sticker_id(app, msg):
     if not msg.reply_to_message:
-        await msg.reply_text("✦ Reply to a sticker")        
+        await msg.reply_text("✦ ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ")        
     elif not msg.reply_to_message.sticker:
-        await msg.reply_text("✦ Reply to a sticker")        
-    st_in = msg.reply_to_message.sticker
-    await msg.reply_text(f"""
+        await msg.reply_text("✦ ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀ sᴛɪᴄᴋᴇʀ")        
+    else:
+        st_in = msg.reply_to_message.sticker
+        await msg.reply_text(f"""
 ❖ sᴛɪᴄᴋᴇʀ ɪɴғᴏ ❖
 
 ● sᴛɪᴄᴋᴇʀ ɪᴅ ➥ {st_in.file_id}\n
 ● sᴛɪᴄᴋᴇʀ ᴜɴɪǫᴜᴇ ɪᴅ ➥ {st_in.file_unique_id}""", reply_markup=InlineKeyboardMarkup(BUTTONS),)
-
